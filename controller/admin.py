3c4da7c577683c7392747851bd8ebd5d784367c3
Andrew Luke <aluke4@byu.edu>
2021-05-20 16:10:27 -0600

    update django admin

--- controller/admin.py
+++ controller/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import requestCount, threshold
+from .models import requestCount, threshold, requestReset, studentWaitTime
 
 # Register your models here.
 admin.site.register(requestCount)
 admin.site.register(threshold)
+admin.site.register(requestReset)
+admin.site.register(studentWaitTime)
